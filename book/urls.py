6fa5d0a4f1ceec3da0da075be518a9b395a2338c
AizatKarimzhanova <aizatkarimova00@gmail.com>
2026-02-10 15:37:26 +0600

    hw4

--- book/urls.py
+++ book/urls.py
@@ -2,7 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("citation/", views.citation_view),
-    path("book_list/", views.book_list),
-    path("book_list/<int:id>/",views.book_detail),
+    path("citation/", views.citation_view, name="citation"),
+
+    # READ
+    path("books/", views.book_list, name="book_list"),
+    path("books/<int:id>/", views.book_detail, name="book_detail"),
+
+    # CREATE
+    path("books/create/", views.book_create, name="book_create"),
+
+    # UPDATE
+    path("books/<int:id>/update/", views.book_update, name="book_update"),
+
+    # DELETE
+    path("books/<int:id>/delete/", views.book_delete, name="book_delete"),
 ]
--- book/views.py
+++ book/views.py
@@ -1,30 +1,90 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from . import models
+from .forms import BookForm
+
 
 def citation_view(request):
-    if request.method == "GET":
-        return HttpResponse("Влюбиться можно в красоту, но полюбить – лишь только душу!(Шекспир)")
+    return HttpResponse("Влюбиться можно в красоту, но полюбить – лишь только душу! (Шекспир)")
 
-def book_detail(request, id):
-    if request.method == "GET":
-        book_id = get_object_or_404(models.Book, id=id)
-        return render(
-            request,
-            "book_details.html",
-            {
-               "book_detail": book_id
-            }
-        )    
-    
+
+# READ: список книг
 def book_list(request):
-    if request.method == "GET":
-        book_list = models.Book.objects.all()
-        return render(
-            request,
-            "book_list.html",
-            {
-                "book_list": book_list
-            }
-        )
-    
+    book_list = models.Book.objects.all()
+    return render(
+        request,
+        "book_list.html",
+        {
+            "book_list": book_list
+        }
+    )
+
+
+# READ: детали книги
+def book_detail(request, id):
+    book = get_object_or_404(models.Book, id=id)
+    return render(
+        request,
+        "book_details.html",
+        {
+            "book_detail": book
+        }
+    )
+
+
+# CREATE: создать книгу
+def book_create(request):
+    if request.method == "POST":
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("book_list")
+    else:
+        form = BookForm()
+
+    return render(
+        request,
+        "book_form.html",
+        {
+            "form": form
+        }
+    )
+
+
+# UPDATE: обновить книгу
+def book_update(request, id):
+    book = get_object_or_404(models.Book, id=id)
+
+    if request.method == "POST":
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect("book_list")
+    else:
+        form = BookForm(instance=book)
+
+    return render(
+        request,
+        "book_form.html",
+        {
+            "form": form
+        }
+    )
+
+
+# DELETE: удалить книгу
+def book_delete(request, id):
+    book = get_object_or_404(models.Book, id=id)
+
+    if request.method == "POST":
+        book.delete()
+        return redirect("book_list")
+
+    return render(
+        request,
+        "book_confirm_delete.html",
+        {
+            "book": book
+        }
+    )
+
